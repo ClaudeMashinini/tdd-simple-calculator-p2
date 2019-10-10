e2db72bfcd49c29061ce47644b823ce3273389cf
Claude Mashinini <dumi.claude@gmail.com>
2019-10-10 13:17:13 +0200

    calculator can handle strings such as LAST and handle slots (SLOT_?) dynamically as long as they've been set

--- calculator.js
+++ calculator.js
@@ -5,19 +5,33 @@ module.exports = class Calculator {
 
   add() {
     let sum = 0;
+
     for (let k = 0; k < arguments.length; k++) {
-      sum += arguments[k];
+      if (arguments[k] != "LAST" && typeof arguments[k] === "string") {
+        sum += this.slots[parseInt(arguments[k].match(/\d+/g)) - 1];
+      } else if (arguments[k] === "LAST") {
+        sum += parseInt(this.last());
+      } else {
+        sum += parseInt(arguments[k]);
+      }
     }
     this.num = sum;
-    return sum;
+    return this.num;
   }
   multiply() {
     let product = 1;
+
     for (let k = 0; k < arguments.length; k++) {
-      product *= arguments[k];
+      if (arguments[k] != "LAST" && typeof arguments[k] === "string") {
+        product *= this.slots[parseInt(arguments[k].match(/\d+/g)) - 1];
+      } else if (arguments[k] === "LAST") {
+        product *= parseInt(this.last());
+      } else {
+        product *= arguments[k];
+      }
     }
     this.num = product;
-    return product;
+    return this.num;
   }
   last() {
     return this.num;
